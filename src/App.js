e9bd7b4cfbf7c917ed78efe08c6ff57abcca1885
srrobin <srrobin07@gmail.com>
2022-12-20 17:53:31 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ShowCode from "./components/ShowCode";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <ShowCode/>
     </div>
   );
 };
--- src/components/ShowCode.jsx
+++ src/components/ShowCode.jsx
@@ -1,3 +1,16 @@
+
+import React, { useState } from 'react';
+import Editor from 'react-simple-code-editor';
+
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+
+import "../style/prism-vsc-dark-plus.css";
+import "../style/editor.css";
+
+const codeSnippet = 
+`
 import React from "react";
 import Hbg from "../images/webb.png";
 
@@ -38,4 +51,39 @@ const Hero = () => {
 };
 
 export default Hero;
-// style={{ backgroundImage: `url(${Hbg})` }}
+
+
+  `
+  ;
+
+
+const ShowCode = () => {
+    const [code, setCode] = useState(codeSnippet);
+    return (
+        <div className="max-w-[1240px] mx-auto my-16 px-4 text-center">
+         <div className="window">
+        <div className="title-bar">
+          <div className="title-buttons">
+            <div className="title-button"></div>
+            <div className="title-button"></div>
+            <div className="title-button"></div>
+          </div>
+        </div>
+        <div className="editor_wrap">
+          <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) => highlight(code, languages.js)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+            }}
+          />
+        </div>
+      </div>
+        </div>
+    );
+};
+
+export default ShowCode;
